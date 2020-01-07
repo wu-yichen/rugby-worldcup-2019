b0e5bd16952c1db9546f7a88e9ce54fff4d4f210
yichen <wuyichen1987@gmail.com>
2020-01-07 20:15:19 +0000

    update .gitignore to exclude the cred

--- ci/setup-pipeline.sh
+++ ci/setup-pipeline.sh
@@ -5,7 +5,7 @@ set -x
 error_message=
 function setup() {
 
-    error_message=$(fly -t wcwhen set-pipeline -n -c pipeline.yml -p wcwhen --load-vars-from .credential.yml 2>&1)
+    error_message=$(fly -t ps set-pipeline -n -c pipeline.yml -p ps --load-vars-from .credential.yml 2>&1)
 }
 
 setup
@@ -13,6 +13,6 @@ if [[ $? -gt 0 ]]; then
     if [[ $error_message =~ "is the targeted Concourse running" ]]; then
         echo "concourse is not started yet, starting now..."
         docker-compose up
-        fly -t wcwhen set-pipeline -c pipeline.yml -p wcwhen --load-vars-from .credential.yml
+        fly -t ps set-pipeline -c pipeline.yml -p ps --load-vars-from .credential.yml
     fi
 fi
